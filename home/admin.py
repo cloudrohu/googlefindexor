1b5105c536862f24328ca051404e08002ef1b149
cloudrohu <cloudrohu@gmail.com>
2024-02-12 10:27:41 +0530

    city

--- home/admin.py
+++ home/admin.py
@@ -4,7 +4,20 @@ from django.contrib import admin
 
 from .models import *
 
+class ImagesInInline(admin.TabularInline):
+    model = Images
+    extra = 1
+    show_change_link = True
 
 
-admin.site.register(Society_Building)
+class Society_BuildingAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'city','locality']
+    list_filter = ['city','locality',]
+    inlines = [ImagesInInline,]
+
+    list_per_page = 25
+
+
+
+admin.site.register(Society_Building,Society_BuildingAdmin)
 admin.site.register(Images)
