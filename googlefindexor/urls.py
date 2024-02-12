99fbaeb9cf2cbe0516742c94d4568341a242eeda
cloudrohu <cloudrohu@gmail.com>
2024-02-12 22:45:44 +0530

    static

--- googlefindexor/urls.py
+++ googlefindexor/urls.py
@@ -1,11 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.contrib import admin
-from django.urls import path,include
-
 urlpatterns = [
+
+    path('jet/', include('jet.urls')),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path('admin/', admin.site.urls),
+    
+
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
