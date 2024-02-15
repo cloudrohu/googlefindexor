31eb6d8e542e65f5a50f0816ff3d6bb60ffb65c8
cloudrohu <cloudrohu@gmail.com>
2024-02-15 21:15:00 +0530

    locality

--- vendor/models.py
+++ vendor/models.py
@@ -51,8 +51,9 @@ class Company(models.Model):
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
     created_by=models.ForeignKey(User, related_name='created_by_user',on_delete=models.CASCADE,null=True,blank=True,)
+
     def __str__(self):
-        return self.company_name
+        return self.company_name + '--' + self.contact_no + '--' + self.locality
     
     class Meta:
         verbose_name_plural='1. Company'
